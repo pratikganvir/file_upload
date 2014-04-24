5b9ff8335f4712f929659007bdc30e0c8ecc6b41
Chaitali Khangar <chaitali.khangar@gmail.com>
2014-04-24 11:29:51 +0530

    Staging key added

--- app/models/user.rb
+++ app/models/user.rb
@@ -77,22 +77,22 @@ class User < ActiveRecord::Base
   end
 
 
-#  #--
-#  # Created By: Chaitali Khangar
-#  # Purpose: To create new session
-#  # Created On: 22/04/2014
-#  #++
-#
-#  def self.new_with_session(params,session)
-#    if session["devise.user_attributes"]
-#      new(session["devise.user_attributes"],without_protection: true) do |user|
-#        user.attributes = params
-#        user.valid?
-#      end
-#    else
-#      super
-#    end
-#  end
+  #--
+  # Created By: Chaitali Khangar
+  # Purpose: To create new session
+  # Created On: 22/04/2014
+  #++
+
+  def self.new_with_session(params,session)
+    if session["devise.user_attributes"]
+      new(session["devise.user_attributes"],without_protection: true) do |user|
+        user.attributes = params
+        user.valid?
+      end
+    else
+      super
+    end
+  end
 
 
   #--
