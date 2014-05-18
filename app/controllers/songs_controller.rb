e64b1afb561be8a6d8b9f865409ef18375579745
Chaitali Khangar <chaitali.khangar@gmail.com>
2014-05-18 15:06:08 +0530

    migration added

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,25 +1,44 @@
 class SongsController < InheritedResources::Base
 
+  before_filter :find_song , :only=>[:edit,:show]
+
+  def index
+    @songs= Song.all
+  end
+
+
+  def show
+
+  end
+  
   def new
     @song = Song.new
   end
 
   def edit
-    @song = Song.find(params[:id])
+    
   end
 
   def create
+    debugger
     @song = Song.new(allowed_params)
     if @song.save
       flash[:notice] = "Song created succesfully."
-      redirect_to :@song
+      debugger
+      redirect_to @song 
     else
       flash[:notice] = @song.errors.full_messages.join("<br/>").html_safe
       render "new"
     end
   end
 
+  protected
+
   def allowed_params
-    params.require(:song).permit(:artist_name,:title,:genere_nam,:duration,:price,:country)
+    params.require(:song).permit(:artist_name,:title,:genere_name,:duration,:price,:country,:song_image,:song_file)
+  end
+
+  def find_song
+    @song = Song.find(params[:id])
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,14 @@
 class Song < ActiveRecord::Base
-  
+  has_attached_file :song_image, :styles => { :small => "150x150>" },
+    :url  => "/assets/songs/images/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/assets/songs/images/:id/:style/:basename.:extension"
+
+   has_attached_file :song_file, :styles => { :small => "150x150>" },
+    :url  => "/assets/songs/file/:id/:style/:basename.:extension",
+    :path => ":rails_root/public/assets/songs/file/:id/:style/:basename.:extension"
+
+  validates_attachment_presence :song_image
+  validates_attachment_presence :song_file
+  validates_attachment_content_type :song_image, :content_type => ['image/jpeg', 'image/png']
+  validates_attachment_presence :song_file,  {:content_type => ["audio/mpeg", "audio/mp3"] }
 end
