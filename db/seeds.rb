3f492b1e2a64a865bd48eeeb613ab4f381ec4d19
Chaitali Khangar <chaitali.khangar@gmail.com>
2014-05-20 11:54:01 +0530

    Changes added of api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-user = User.new(:name=> 'Admin Admin',:email => 'admin@admin.com', :password => 'Restrict29Pass', :password_confirmation => 'Restrict29Pass')
+user = User.new(:name=> 'Admin Admin',:email => 'admin@admin.com', :password => 'Restrict29Pass', :password_confirmation => 'Restrict29Pass',:status=>"Active")
 user.build_role(:role_name=>"Admin")
 user.skip_confirmation!
 user.save
